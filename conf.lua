7471043bf242bcb4b2b48d6a2242e681d10f2b11
Tammpere, Laura <ltammpere@ea.com>
2018-11-03 21:18:02 -0700

    added styling

--- conf.lua
+++ conf.lua
@@ -1,34 +1,48 @@
 function love.conf(t)
-	t.console = true
-	window_conf()
+    t.console = true
+    
+    score = 0    
+    score_colour = {0.6039215686274509, 
+                    0.5098039215686274, 
+                    0.6509803921568628}
+
+    window_conf()
 	snake_conf()
 	food_conf()
 end
 
 function window_conf()
-	window_width = 800
-	window_height = 500
+    window_height = 500
+    window_width = 800
 end
 
 function snake_conf()
 	snake_speed = 200
-	snake_radius = 8
+    snake_radius = 8
+    snake_colour =  {79/255, 114/255, 111/255}
+    snake_head_width = 1.5
+    snake_width = 1
 
     -- initialize snake
     x = window_width / 2
     y = window_height / 2
     snake = {
         {x, y},
-        {x-2*snake_radius, y},
-        {x-4*snake_radius, y}
+        {x- 2 * snake_radius, y},
+        {x - 4 * snake_radius, y}
     }
 end
 
 function food_conf()
-    food_radius = 10
-    food_timer = 0
-    food_interval = 5
     foods = {}
+    food_alpha = 0.5
+    food_fadein_speed = 0.30
+    food_max_radius = 8
+    food_min_radius = 4
+    food_hitbox_buffer = 2
+    food_animation_speed = 3
+    food_interval = 5
+    food_drop_rate = 500
     food_colours = {
         {0.8627450980392157, 0.7294117647058823, 0.4},
         {0.8627450980392157, 0.7294117647058823, 0.5333333333333333},
--- main.lua
+++ main.lua
@@ -1,31 +1,44 @@
 function love.load()
     love.window.setMode(window_width, window_height)
+    love.graphics.setBackgroundColor{32/255, 42/255, 48/255}
+    love.graphics.setFont(love.graphics.newFont(20))
 end
 
 function love.update(dt)
      register_movement(dt)
      eat_food()
      place_food(dt)
+     animate_food(dt)
 end
 
 function love.draw()
+    draw_score()
     draw_food()
     draw_snake()
+end
+
+function draw_score()
+    love.graphics.setColor(score_colour)
+    love.graphics.print(score, window_width - 50, 25)
 end
 
 function draw_food()    
     for i,food in ipairs(foods) do
         love.graphics.setColor(food[3])   
         love.graphics.setLineWidth(0.2)     
-        love.graphics.circle("line", food[1], food[2], food_radius)
+        love.graphics.circle("line", food[1], food[2], food[4])
     end
 end
 
 function draw_snake()
-    love.graphics.setColor(79/255, 114/255, 111/255, 1)
+    love.graphics.setColor(snake_colour) -- remove alpha
+
     -- render head
-    love.graphics.circle("fill", snake[1][1], snake[1][2], snake_radius)
+    love.graphics.setLineWidth(snake_head_width)
+    love.graphics.circle("line", snake[1][1], snake[1][2], snake_radius)
+
     -- render body
+    love.graphics.setLineWidth(snake_width)     
     if #snake > 1 then
         for i=2, #snake do
             love.graphics.circle("line", snake[i][1], snake[i][2], snake_radius)
@@ -36,7 +49,8 @@ end
 -- Food functions
 function eat_food()
     for i,food in ipairs(foods) do
-        if collides(snake[1], food, food_radius+food_radius*2) then 
+        if collides(snake[1], food, food_max_radius + food_max_radius * food_hitbox_buffer) then 
+           score = score + 1 + math.floor(foods[i][4])-food_min_radius
            table.remove(foods, i)
            -- Todo: determine starting location for new body segment
            --       in more sophisticated way
@@ -54,20 +68,44 @@ function collides(collider, collidee, r)
 end
 
 function place_food(dt)
-    food_timer = food_timer + dt
+    time_to_eat = love.math.random(0, food_drop_rate) == 0
 
-    -- Randomly determine food colour
-    food_colour = food_colours[love.math.random(#food_colours)]
-    alpha = (1/(love.math.random(2,5)))+0.5    
-    table.insert(food_colour, 0.5)
+    if time_to_eat then 
+        -- Randomly determine food colour and starting size
+        food_colour = food_colours[love.math.random(#food_colours)]
+        table.insert(food_colour, 0)
+        food_size = math.random(food_max_radius / 2, food_max_radius)
 
-    if food_timer >= food_interval then 
-        table.insert(foods, {love.math.random(food_radius, window_width-food_radius), 
-                             love.math.random(food_radius, window_height-food_radius),
-                             food_colour
+        table.insert(foods, {love.math.random(food_max_radius, window_width - food_max_radius), 
+                             love.math.random(food_max_radius, window_height - food_max_radius),
+                             food_colour,
+                             food_size,
+                             1 -- multiplier for animation
                             }
                     )
-        food_timer = food_timer - food_interval
+    end
+end
+
+function animate_food(dt)
+    for i, food in ipairs(foods) do
+        -- animate fade-in
+        current_alpha = food[3][4]
+        if current_alpha < food_alpha then
+            next_alpha = current_alpha + (dt * food_fadein_speed)
+            if next_alpha < food_alpha then food[3][4] = next_alpha else food[3][4] = food_alpha end
+        end
+
+        -- animate size
+        current_radius = food[4]
+        next_radius = current_radius + (food[5] * food_animation_speed * dt)
+        if next_radius < food_min_radius then 
+            next_radius = food_min_radius + (food_min_radius - next_radius)
+            food[5] = food[5] * -1  
+        elseif next_radius > food_max_radius then 
+            next_radius = food_max_radius - (next_radius - food_max_radius)
+            food[5] = food[5] * -1
+        end
+        food[4] = next_radius
     end
 end
 
